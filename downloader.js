f016837c6392b187a896bc3b212e42c0c291795f
L3m <96365455+L3m0n-Cao@users.noreply.github.com>
2022-12-06 20:32:26 +0700

    add shitty homepage to download

---
+++ downloader.js
@@ -0,0 +1,29 @@
+const apps = {
+    // Android: "https://transsocial.software/compiled/1.0/android/",
+    // iOS: "https://transsocial.software/compiled/1.0/ios/",
+    Windows: "https://transsocial.software/compiled/1.0/windows/",
+    // Mac: "https://transsocial.software/compiled/1.0/macos/",
+    // Linux: "https://transsocial.software/compiled/1.0/linux/"
+  };
+  
+  const platform = () => {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/windows phone/i.test(userAgent)) return "Windows Phone";
+    if (/android/i.test(userAgent)) return "Android";
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) return "iOS";
+    if (navigator.appVersion.indexOf("Win")!=-1) return "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) return "Mac";;
+    if (navigator.appVersion.indexOf("Linux")!=-1) return "Linux";
+    return null;
+  };
+  
+  const redirect = () => {
+    let os = platform();
+    if (os in apps) {
+      location.replace(apps[os]);
+    } else {
+      location.replace("https://transsocial.software/compiled/1.0/windows/");
+    }
+  };
+  
+  redirect();
--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-const apps = {
-    Android: "https://transsocial.software/compiled/1.0/android/",
-    iOS: "https://transsocial.software/compiled/1.0/ios/",
-    Windows: "https://transsocial.software/compiled/1.0/windows/",
-    Mac: "https://transsocial.software/compiled/1.0/macos/",
-    Linux: "https://transsocial.software/compiled/1.0/linux/"
-  };
-  
-  const platform = () => {
-    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (/windows phone/i.test(userAgent)) return "Windows Phone";
-    if (/android/i.test(userAgent)) return "Android";
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) return "iOS";
-    if (navigator.appVersion.indexOf("Win")!=-1) return "Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) return "Mac";;
-    if (navigator.appVersion.indexOf("Linux")!=-1) return "Linux";
-    return null;
-  };
-  
-  const redirect = () => {
-    let os = platform();
-    if (os in apps) {
-      location.replace(apps[os]);
-    } else {
-      const message = document.querySelector(".message");
-      message.innerText = "Your OS is not supported currently, or you may need to update your browser.";
-    }
-  };
-  
-  redirect();
+function dl(){
+    var event = new CustomEvent("event", { "detail": "event" });
+    document.dispatchEvent(event);
+}
+document.addEventListener("event", function(){
+    var headID = document.getElementsByTagName("head")[0];         
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    var scriptsrc = "downloader.js?" + Math.floor(Math.random()*100);
+    newScript.src = scriptsrc;
+    headID.appendChild(newScript);
+});
